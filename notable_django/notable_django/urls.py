b78cae4b7b653da7480eef01a9b444565c8f7857
Aktar Zaman <Auz.zamy@gmail.com>
2019-08-13 18:44:22 -0400

    first commit

---
+++ notable_django/notable_django/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path
+from django.conf.urls import url, include
+from api.resources import NoteResource
+
+note_resource = NoteResource()
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include(note_resource.urls)),
+]
